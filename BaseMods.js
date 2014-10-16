8969b6562f1990cb4cdd54a9525a3926fcaf9837
PluginLearner <itsminecrafttime32@gmail.com>
2014-10-15 21:06:16 -0700

    Im Done with CrooksCraft!! i just now need too add Ore Spawns Block
    CrooksCraft: Done
    OreSpawnsBlock: Not Done
    Barrels: Not Done
    AutoSmelter: Not Done

---
+++ BaseMods.js
@@ -0,0 +1,41 @@
+// I wanted To Point Out This is not SkyFactory its my edition of something similar to sky factory ill explain more int another file ;)
+
+
+ModPE.setItem(400, "hoe", 0, "Wooden Crook");
+
+ModPE.setItem(401, "hoe", 1, "Stone Crook");
+
+ModPE.setItem(402, "hoe", 2, "Iron Crook");
+
+ModPE.setItem(403, "hoe", 3, "Golden Crook");
+
+ModPE.setItem(404, "hoe", 4, "Diamond Crook");
+
+function useItem(x, y, z, itemId, blockId, side) {
+    if (itemId== 400) {
+        addItemInventory(6, 1, 0);
+    }
+    
+function useItem(x, y, z, itemId, blockId, side) {
+    if (itemId== 401) {
+        addItemInventory(6, 2, 0);
+    }
+    
+function useItem(x, y, z, itemId, blockId, side) {
+    if (itemId== 402) {
+        addItemInventory(6, 4, 0);
+    }
+    
+function useItem(x, y, z, itemId, blockId, side) {
+    if (itemId== 403) {
+        addItemInventory(6, 3, 0);
+    }
+    
+function useItem(x, y, z, itemId, blockId, side) {
+    if (itemId== 404) {
+        addItemInventory(6, 10, 0);
+        
+    }
+    
+Block.defineBlock(id,"Ore Spawns Block"
+}
